be26b97fe9a7e0f88b70b9a86c0709724648cc06
Nechval <93921142+Nechval@users.noreply.github.com>
2021-11-09 13:18:44 +0200

    Add files via upload

---
+++ scratch_2.py
@@ -0,0 +1,14 @@
+first = 10
+second=30
+Addition= first+second
+print(Addition)
+Subtraction= first-second
+print(Subtraction)
+multiplication=first*second
+print(multiplication)
+division=first/second
+print(division)
+Exponentiation=first**second
+print(Exponentiation)
+remainder=first%second
+print(remainder)
